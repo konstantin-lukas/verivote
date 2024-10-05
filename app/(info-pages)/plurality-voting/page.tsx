ff6965916fb5e3fd164f0fd84bbcdf7d317c7ab7
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-05 22:45:07 +0200

    add meta data to info pages

--- app/(info-pages)/plurality-voting/page.tsx
+++ app/(info-pages)/plurality-voting/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next";
 import React from "react";
 
 import InfoCard from "@/components/routes/(info-pages)/InfoCard";
 import type { VotingMethod } from "@/data/types";
 import { votingMethods } from "@/data/votingMethods";
+
+export const metadata: Metadata = {
+    title: "Plurality Voting - Verivote",
+    description: votingMethods.find(x => x.name === "Plurality Voting")?.shortDescription,
+};
 
 export default async function Page() {
     return (
