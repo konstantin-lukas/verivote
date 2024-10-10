bba533410f2afbd929c123ac96d83ce5abe50c5f
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-10 12:07:18 +0200

    add delete button to poll options

--- components/routes/create/CreationMenu.tsx
+++ components/routes/create/CreationMenu.tsx
@@ -6,6 +6,7 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { MobileDateTimePicker } from "@mui/x-date-pickers/MobileDateTimePicker";
 import React, { useMemo, useReducer } from "react";
+import { IoMdClose } from "react-icons/io";
 import { IoAddSharp } from "react-icons/io5";
 
 import Checkbox from "@/components/form/Checkbox";
@@ -32,6 +33,11 @@ function reducer(
         copy.options.push("");
         return copy;
     }
+    if (type === "optionsDelete") {
+        const copy = { ...state, options: [...state.options]};
+        copy.options.splice(index as number, 1);
+        return copy;
+    }
 
     return { ...state };
 }
@@ -48,19 +54,27 @@ export default function CreationMenu() {
     const options = useMemo(() => {
         return state.options.map((o, i) => {
             return (
-                <Input
-                    value={o}
-                    key={i}
-                    setValue={value => dispatch({ type: "optionsChange", value, index: i })}
-                    placeholder={"Option " + (i + 1)}
-                    className="mt-4"
-                />
+                <div key={i} className="relative mt-4">
+                    <Input
+                        value={o}
+                        setValue={value => dispatch({ type: "optionsChange", value, index: i })}
+                        placeholder={"Option " + (i + 1)}
+                    />
+                    {i > 1 && (
+                        <button
+                            className="group absolute right-4 top-1/2 -translate-y-1/2"
+                            onClick={() => dispatch({ type: "optionsDelete", index: i })}
+                        >
+                            <IoMdClose className="size-6 transition-colors group-hover:text-rose-500"/>
+                        </button>
+                    )}
+                </div>
             );
         });
     }, [state]);
 
     return (
-        <div className="mx-auto mt-24 inline-flex flex-col">
+        <div className="mx-auto my-24 inline-flex flex-col">
             <Dropdown
                 options={votingMethods.map(m => m.name)}
                 defaultOption={0}
@@ -86,13 +100,19 @@ export default function CreationMenu() {
                 label="Winner needs majority: "
             />
             {options}
-            <button
-                className="group mx-auto mt-6 flex size-10 items-center justify-center rounded-full shadow-3d
+            { state.options.length < parseInt(process.env.NEXT_PUBLIC_MAX_OPTIONS_PER_POLL ?? "20") && (
+                <button
+                    className="group mx-auto mt-6 flex size-10 items-center justify-center rounded-full shadow-3d
                 transition-shadow hover:shadow-3d-both dark:shadow-dark-3d dark:hover:shadow-dark-3d-both"
-                onClick={() => dispatch({ type: "optionsAdd" })}
-            >
-                <IoAddSharp className="size-7 transition-transform group-hover:scale-90"/>
-            </button>
+                    onClick={() => {
+                        dispatch({ type: "optionsAdd" });
+                        window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+                    }}
+                >
+                    <IoAddSharp className="size-7 transition-transform group-hover:scale-90"/>
+                </button>
+            )}
+
         </div>
     );
 }
