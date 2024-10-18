74b343642764c3d615f1b5bd496d1a832012a0ed
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-18 22:41:10 +0200

    fix css issue that caused h1 elements to be cut off at the bottom

--- components/shared/H1.tsx
+++ components/shared/H1.tsx
@@ -5,8 +5,8 @@ export default function H1({ children, className, customSizes }: { children: Rea
     const sizes = customSizes ?? "text-4xl sm:text-5xl md:text-6xl";
     return (
         <h1
-            className={`mb-2 inline-block bg-gradient-to-r from-verivote-turquoise to-verivote-cyan
-                bg-clip-text font-bold uppercase text-transparent ${className ?? ""} ${sizes}`}
+            className={`inline-block bg-gradient-to-r from-verivote-turquoise to-verivote-cyan bg-clip-text pb-2
+            font-bold uppercase text-transparent ${className ?? ""} ${sizes}`}
         >
             {children}
         </h1>
