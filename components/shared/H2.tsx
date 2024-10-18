e371731cdf1f812173c78e7b9a3e257331223c67
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-18 22:41:41 +0200

    fix css issue that caused h2 elements to be cut off at the bottom

--- components/shared/H2.tsx
+++ components/shared/H2.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function H2({ children }: { children: ReactNode }) {
     return (
         <h2
-            className="mb-2 inline-block bg-gradient-to-r from-verivote-turquoise to-verivote-cyan
-            bg-clip-text text-2xl font-bold uppercase text-transparent sm:text-3xl"
+            className="inline-block bg-gradient-to-r from-verivote-turquoise to-verivote-cyan bg-clip-text
+            pb-2 text-2xl font-bold uppercase text-transparent sm:text-3xl"
         >
             {children}
         </h2>
