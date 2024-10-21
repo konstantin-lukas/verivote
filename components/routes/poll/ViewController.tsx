7e5836362994ed1983c66de2fe4406f1d0c83b35
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-21 17:40:18 +0200

    add closing date verification to front end

--- components/routes/poll/ViewController.tsx
+++ components/routes/poll/ViewController.tsx
@@ -51,10 +51,12 @@ export default function ViewController({ poll, results, info, defaultHasVoted }:
             <Wrapper className="flex  flex-col items-center">
                 <H1 customSizes="text-2xl sm:text-3xl md:text-4xl" className="text-center">{poll.name}</H1>
                 <h2 className="mb-2 text-lg font-bold uppercase text-dark-font sm:text-xl md:text-2xl dark:text-light-font">{info.name}</h2>
-                <span className="text-center text-neutral-500">Closing Time: {formatDate(date)}</span>
+                <span className="text-center text-neutral-500">
+                    {date >= new Date() ? "Closing time" : "This poll ended on"}: {formatDate(date)}
+                </span>
                 <div className="mt-6 flex flex-wrap justify-center gap-6">
                     <ShareButton url="https://google.com"/>
-                    {!showResults && !hasVoted && (
+                    {date >= new Date() && !showResults && !hasVoted && (
                         <BlockButton className="flex items-center justify-center" onClick={() => setShowResults(true)}>
                             <LuPieChart className="mr-1 inline-block size-4 translate-y-[-.1rem]"/>
                             <span>
@@ -62,7 +64,7 @@ export default function ViewController({ poll, results, info, defaultHasVoted }:
                             </span>
                         </BlockButton>
                     )}
-                    {showResults && !hasVoted && (
+                    {date >= new Date() && showResults && !hasVoted && (
                         <BlockButton className="flex items-center justify-center" onClick={() => setShowResults(false)}>
                             <MdOutlineHowToVote className="mr-1 inline-block size-4 translate-y-[-.1rem]"/>
                             <span>
@@ -73,19 +75,19 @@ export default function ViewController({ poll, results, info, defaultHasVoted }:
                 </div>
             </Wrapper>
             <WrapperSmall>
-                {!hasVoted && !showResults && ["Instant-Runoff", "Positional Voting"].includes(info.name) &&
+                {date >= new Date() && !hasVoted && !showResults && ["Instant-Runoff", "Positional Voting"].includes(info.name) &&
                     <RankedVoting poll={poll} setHasVoted={setHasVoted}/>
                 }
-                {!hasVoted && !showResults && info.name === "Score Voting" &&
+                {date >= new Date() && !hasVoted && !showResults && info.name === "Score Voting" &&
                     <ScoreVoting poll={poll} setHasVoted={setHasVoted}/>
                 }
-                {!hasVoted && !showResults && info.name === "Approval Voting" &&
+                {date >= new Date() && !hasVoted && !showResults && info.name === "Approval Voting" &&
                     <ApprovalVoting poll={poll} setHasVoted={setHasVoted}/>
                 }
-                {!hasVoted && !showResults && info.name === "Plurality Voting" &&
+                {date >= new Date() && !hasVoted && !showResults && info.name === "Plurality Voting" &&
                     <PluralityVoting poll={poll} setHasVoted={setHasVoted}/>
                 }
-                {(hasVoted || showResults) && <PollResults poll={poll} results={pollResults}/>}
+                {(date < new Date() || hasVoted || showResults) && <PollResults poll={poll} results={pollResults}/>}
             </WrapperSmall>
         </>
     );
