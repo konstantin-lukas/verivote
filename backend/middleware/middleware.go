12fc969371edad9778537afba1d4b5e466ce4049
Konstantin Lukas <konstantin_lukas@web.de>
2024-10-22 16:18:41 +0200

    update cors middleware

--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -87,14 +87,14 @@ func Authorize(next http.Handler) http.Handler {
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", os.Getenv("CORS_ALLOW_ORIGIN"))
-		w.Header().Add("Access-Control-Allow-Credentials", "true")
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		w.Header().Add("Access-Control-Expose-Headers", "Location")
-		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CORS_ALLOW_ORIGIN"))
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 		if r.Method == "OPTIONS" {
-			http.Error(w, "No Content", http.StatusNoContent)
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
