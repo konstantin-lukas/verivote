6483c3113d14724bd0ec1abc665f538e4f6690a1
konstantin <konstantin_lukas@web.de>
2025-01-18 14:48:50 +0100

    add next auth

---
+++ src/app/auth/[...nextauth]/route.tsx
@@ -0,0 +1,35 @@
+import type { AuthOptions, Session } from "next-auth";
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import RedditProvider from "next-auth/providers/reddit";
+
+const authOptions: AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID ?? "",
+            clientSecret: process.env.GITHUB_SECRET ?? "",
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_ID ?? "",
+            clientSecret: process.env.DISCORD_SECRET ?? "",
+        }),
+        RedditProvider({
+            clientId: process.env.REDDIT_ID ?? "",
+            clientSecret: process.env.REDDIT_SECRET ?? "",
+        }),
+    ],
+    // pages: {
+    //     signIn: "/signin",
+    // },
+    callbacks: {
+        async session({ session }: { session: Session }) {
+            delete session.user;
+            return session;
+        },
+    },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
