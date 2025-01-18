873577c0ad239e30caab2152a67935ab14994a36
konstantin <konstantin_lukas@web.de>
2025-01-18 17:57:57 +0100

    implement login

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,45 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import React from "react";
+import { PiRedditLogo } from "react-icons/pi";
+import { RiDiscordLine, RiGithubLine } from "react-icons/ri";
+
+import SignInButton from "@/components/routes/auth/SignInButton";
+import Wrapper from "@/components/shared/Wrapper";
+
+export default async function SignIn(context: {
+    searchParams: Promise<{ callbackUrl: string }>;
+}) {
+    const session = await getServerSession();
+    if (session?.user)
+        redirect((await context.searchParams)?.callbackUrl ?? "/");
+    return (
+        <div className="flex min-h-[var(--main-height-mobile)] items-center justify-center desktop:min-h-[var(--main-height)]">
+            <Wrapper>
+                <div
+                    className="mx-auto box-border flex max-w-72 flex-col items-center justify-center p-8 shadow-3d-inset
+                    dark:shadow-dark-3d-inset"
+                >
+                    <span
+                        className="inline-block bg-gradient-to-r from-verivote-turquoise to-verivote-cyan bg-clip-text
+                    text-3xl font-bold uppercase text-transparent"
+                    >
+                        Sign In
+                    </span>
+                    <SignInButton serviceName="discord">
+                        <RiDiscordLine className="mr-2 inline" />
+                        <span>Discord</span>
+                    </SignInButton>
+                    <SignInButton serviceName="github">
+                        <RiGithubLine className="mr-2 inline" />
+                        <span>GitHub</span>
+                    </SignInButton>
+                    <SignInButton serviceName="reddit">
+                        <PiRedditLogo className="mr-2 inline" />
+                        <span>Reddit</span>
+                    </SignInButton>
+                </div>
+            </Wrapper>
+        </div>
+    );
+}
---
+++ src/components/routes/auth/SignInButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import type { ReactNode } from "react";
+import React from "react";
+
+import ButtonLink from "@/components/shared/BlockButton";
+
+export default function SignInButton({
+    serviceName,
+    children,
+}: {
+    serviceName: string;
+    children: ReactNode;
+}) {
+    return (
+        <ButtonLink
+            onClick={() => signIn(serviceName)}
+            className="mt-6 flex translate-y-[-0.1rem] flex-row items-center justify-center"
+        >
+            {children}
+        </ButtonLink>
+    );
+}
