d6e6e6b4f0dc48790c82fd494fbf3c3eeb542332
konstantin <konstantin_lukas@web.de>
2025-05-08 22:33:00 +0200

    fix ci.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
     testDir: "./tests/e2e",
     fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    forbidOnly: process.env.APP_ENV === "test",
+    retries: process.env.APP_ENV === "test" ? 2 : 0,
+    workers: process.env.APP_ENV === "test" ? 1 : undefined,
     reporter: "html",
     use: {
         baseURL: process.env.APP_ENV === "test" ? "http://webserver:3000" : "http://localhost:3000",
